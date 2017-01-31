26ca314fc2775578e9dce0649870229f3877a185
hihumi <yui.maa5800@gmail.com>
2017-01-31 16:56:35 +0900

    add file

---
+++ print_word/template.py
@@ -0,0 +1,23 @@
+#!/use/bin/env python3
+
+
+"""xxxの日本語意味を出力する
+"""
+
+def xxx():
+    """xxxの日本語意味を出力する関数
+
+    doctest:
+    >>> xxx()
+    (test内容)
+    """
+
+    x_ja = """
+"""
+
+    print(x_ja)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    xxx()
