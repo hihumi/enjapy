0565692b2bc640113dc5a5c314262ff5f28eb8f3
hihumi <yui.maa5800@gmail.com>
2017-01-31 16:56:11 +0900

    update

--- re_word/re_word.py
+++ re_word/re_word.py
@@ -7,7 +7,12 @@
 
 import re
 
+# a b c d e f g h i j k l m n o p q r s t u v w x y z
+# r
+from print_word import respect
+# y
 from print_word import you
+# m
 from print_word import man
 
 
@@ -16,12 +21,75 @@ def re_word(word):
     dir: print_wordにある各関数を呼び出す。
     """
 
+    # a b c d e f g h i j k l m n o p q r s t u v w x y z
+    #
+    # a
+    # b
+    # c
+    # d
+    # e
+    # f
+    # g
+    # h
+    # i
+    # j
+    # k
+    # l
+    # m
     man_re = re.compile(r'man$', re.I)
+
+    # n
+    # o
+    # p
+    # q
+    # r
+    respect_re = re.compile(r'respect$', re.I)
+    # s
+    # t
+    # u
+    # v
+    # w
+    # x
+    # y
     you_re = re.compile(r'you$', re.I)
 
+    # z
+
+    #
+    # a b c d e f g h i j k l m n o p q r s t u v w x y z
+    #
+    # a
+    # b
+    # c
+    # d
+    # e
+    # f
+    # g
+    # h
+    # i
+    # j
+    # k
+    # l
+    # m
     if man_re.search(word):
         man.man()
+    # n
+    # o
+    # p
+    # q
+    # r
+    elif respect_re.search(word):
+        respect.respect()
+    # s
+    # t
+    # u
+    # v
+    # w
+    # x
+    # y
     elif you_re.search(word):
         you.you()
+    # z
+    #
     else:
         print('not found...')
