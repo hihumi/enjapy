2369af32d52a3a317bd53220f1e60df82f52a351
hihumi <yui.maa5800@gmail.com>
2017-01-31 16:55:34 +0900

    add file

---
+++ print_word/respect.py
@@ -0,0 +1,24 @@
+#!/use/bin/env python3
+
+
+"""respectの日本語意味を出力する
+"""
+
+def respect():
+    """respectの日本語意味を出力する関数
+
+    doctest:
+    >>> respect()
+    [動]: (1) ...を尊重する (2) ...を尊敬する
+    [名]: [不可算]: (1) 尊敬
+    """
+
+    r_ja = """[動]: (1) ...を尊重する (2) ...を尊敬する
+[名]: [不可算]: (1) 尊敬"""
+
+    print(r_ja)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    respect()
