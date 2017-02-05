8c1abf5ac1f4699eed40a4e20f8d6046e878c874
hihumi <yui.maa5800@gmail.com>
2017-02-05 11:19:48 +0900

    update

--- re_en/a_re.py
+++ re_en/a_re.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
 
+"""aの正規表現をコンパイルする
+"""
+
+
 import re
 
 
 def a_re_func():
+    """aの正規表現をコンパイルする関数
+
+    return value: a_re
+
+    doctest:
+    >>> print(a_re_func())
+    re.compile('a$', re.IGNORECASE)
+    """
+
     a_re = re.compile(r'a$', re.I)
     return a_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(a_re_func())
