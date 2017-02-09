f59101f979abcb190a6bca02829f108043d30609
hihumi <yui.maa5800@gmail.com>
2017-02-09 12:30:02 +0900

    add file

---
+++ print_ja/assure_ja.py
@@ -0,0 +1,26 @@
+#!/use/bin/env python3
+
+
+"""assureの日本語意味を出力する
+"""
+
+
+def assure_ja_func():
+    """assureの日本語意味を出力する関数
+
+    doctest:
+    >>> assure_ja_func()
+    assure:
+    [動]: (1) ...を確実にする (2) ...を保証する
+    """
+
+    print('assure:')
+
+    assure_ja_words = """[動]: (1) ...を確実にする (2) ...を保証する"""
+
+    print(assure_ja_words)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    assure_ja_func()
