388e15171140ea72094698b519b96647ebd61480
hihumi <yui.maa5800@gmail.com>
2017-02-12 06:32:50 +0900

    add file

---
+++ print_ja/assurance_ja.py
@@ -0,0 +1,26 @@
+#!/use/bin/env python3
+
+
+"""assuranceの日本語意味を出力する
+"""
+
+
+def assurance_ja_func():
+    """assuranceの日本語意味を出力する関数
+
+    doctest:
+    >>> assurance_ja_func()
+    assurance:
+    [名]: [加算・不加算]: (1) 確約 (2) 保証
+    """
+
+    print('assurance:')
+
+    assurance_ja_words = """[名]: [加算・不加算]: (1) 確約 (2) 保証"""
+
+    print(assurance_ja_words)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    assurance_ja_func()
