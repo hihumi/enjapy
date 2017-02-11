c46614c48405fdda22b98fd30153f4a98cc247ab
hihumi <yui.maa5800@gmail.com>
2017-02-12 07:58:46 +0900

    add file

---
+++ re_en/outlook_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""outlookの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def outlook_re_func():
+    """outlookの正規表現をコンパイルする関数
+
+    return value: outlook_re
+
+    doctest:
+    >>> print(outlook_re_func())
+    re.compile('outlook$', re.IGNORECASE)
+    """
+
+    outlook_re = re.compile(r'outlook$', re.I)
+    return outlook_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(outlook_re_func())
