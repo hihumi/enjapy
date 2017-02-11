fe627b711925186eb4aef52ce19dcd500ec1fd67
hihumi <yui.maa5800@gmail.com>
2017-02-12 07:00:27 +0900

    add file

---
+++ re_en/negative_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""negativeの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def negative_re_func():
+    """negativeの正規表現をコンパイルする関数
+
+    return value: negative_re
+
+    doctest:
+    >>> print(negative_re_func())
+    re.compile('negative$', re.IGNORECASE)
+    """
+
+    negative_re = re.compile(r'negative$', re.I)
+    return negative_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(negative_re_func())
