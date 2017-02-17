8df2eaea56e746f9bd4d7bbc548178082177378f
hihumi <yui.maa5800@gmail.com>
2017-02-17 10:55:38 +0900

    add file

---
+++ docs/enjapy_doc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+"""enjapy main doc
+"""
+
+
+__author__ = "hihumi <mail@example.com>"
+__version__ = '0.0.1'
+__date__ = '05 Feb 2017'
+__status__ = 'product under development'
+
+def enjapy_main_doc():
+    pass
+
+if __name__ == '__main__':
+    enjapy_main_doc()
