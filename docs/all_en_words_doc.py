65f025001f7118aa167d45022dee8793af374753
hihumi <yui.maa5800@gmail.com>
2017-02-17 10:56:05 +0900

    add file

---
+++ docs/all_en_words_doc.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+
+"""全英単語リスト
+"""
+
+
+__author__ = "hihumi <mail@example.com>"
+__version__ = '0.0.1'
+__date__ = '05 Feb 2017'
+__status__ = 'product under development'
+
+
+def all_en_words():
+    """a:
+assure
+assurance
+
+i:
+individual
+individuality
+
+m:
+maintain
+
+n:
+negative:
+
+o:
+outlook
+
+r:
+respect
+respectable
+respectful
+
+w:
+will
+willpower
+"""
+
+    # a b c d e f g h i j k l m n o p q r s t u v w x y z
+    all_en = """a:
+assure
+assurance
+
+i:
+individual
+individuality
+
+m:
+maintain
+
+n:
+negative:
+
+o:
+outlook
+
+r:
+respect
+respectable
+respectful
+
+w:
+will
+willpower
+"""
+
+    print(all_en)
+
+if __name__ == '__main__':
+    all_en_words()
