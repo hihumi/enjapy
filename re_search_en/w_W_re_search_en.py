b22b4e21b28ac331bbb5bb72b5971f480a483941
hihumi <yui.maa5800@gmail.com>
2017-03-16 03:48:52 +0900

    update

--- re_search_en/w_W_re_search_en.py
+++ re_search_en/w_W_re_search_en.py
@@ -10,6 +10,8 @@ wxxx_jaモジュールのwxxx_ja_func()を呼ぶ
 # w: a b c d e f g h i j k l m n o p q r s t u v w x y z
 #
 # from re_en.w_re import w_re_func
+# want
+from re_en.want_re import want_re_func
 # will:
 from re_en.will_re import will_re_func
 # willpower:
@@ -18,6 +20,8 @@ from re_en.willpower_re import willpower_re_func
 # w: a b c d e f g h i j k l m n o p q r s t u v w x y z
 #
 # from print_ja.w_ja import w_ja_func
+# want:
+from print_ja.want_ja import want_ja_func
 # will:
 from print_ja.will_ja import will_ja_func
 # willpower:
@@ -31,7 +35,9 @@ def w_W_re_search_en_func(word):
     """
 
     # w:
-    if will_re_func().search(word): # will
+    if want_re_func().search(word): # want
+        want_ja_func()
+    elif will_re_func().search(word): # will
         will_ja_func()
     elif willpower_re_func().search(word): # willpower
         willpower_ja_func()
