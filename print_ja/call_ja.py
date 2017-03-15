160c0ca0c70e6a5bb74b64abe7b1ed32ca462299
hihumi <yui.maa5800@gmail.com>
2017-03-16 05:07:19 +0900

    add file

---
+++ print_ja/call_ja.py
@@ -0,0 +1,28 @@
+#!/use/bin/env python3
+
+
+"""callの日本語意味を出力する
+"""
+
+
+def call_ja_func():
+    """callの日本語意味を出力する関数
+
+    doctest:
+    >>> call_ja_func()
+    call:
+    [動]: (1) 呼ぶ (2) 電話を掛ける
+    [名]: [加算]: (1) 呼び出し (2) 通話
+    """
+
+    print('call:')
+
+    call_ja_words = """[動]: (1) 呼ぶ (2) 電話を掛ける
+[名]: [加算]: (1) 呼び出し (2) 通話"""
+
+    print(call_ja_words)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    call_ja_func()
---
+++ re_en/call_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""callの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def call_re_func():
+    """callの正規表現をコンパイルする関数
+
+    return value: call_re
+
+    doctest:
+    >>> print(call_re_func())
+    re.compile('call$', re.IGNORECASE)
+    """
+
+    call_re = re.compile(r'call$', re.I)
+    return call_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(call_re_func())
