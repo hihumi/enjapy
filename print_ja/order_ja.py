78790b033a9429be550b3d5d5fa595fc7c61702d
hihumi <yui.maa5800@gmail.com>
2017-03-16 06:58:24 +0900

    add file

---
+++ print_ja/order_ja.py
@@ -0,0 +1,28 @@
+#!/use/bin/env python3
+
+
+"""orderの日本語意味を出力する
+"""
+
+
+def order_ja_func():
+    """orderの日本語意味を出力する関数
+
+    doctest:
+    >>> order_ja_func()
+    order:
+    [名]: [加算]: (1) 注文 (2) 命令 [不加算]: (1) 秩序 (2) 順序
+    [動]: (1) (モノ等)を注文する (2) ...を命令する
+    """
+
+    print('order:')
+
+    order_ja_words = """[名]: [加算]: (1) 注文 (2) 命令 [不加算]: (1) 秩序 (2) 順序
+[動]: (1) (モノ等)を注文する (2) ...を命令する"""
+
+    print(order_ja_words)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    order_ja_func()
---
+++ re_en/order_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""orderの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def order_re_func():
+    """orderの正規表現をコンパイルする関数
+
+    return value: order_re
+
+    doctest:
+    >>> print(order_re_func())
+    re.compile('order$', re.IGNORECASE)
+    """
+
+    order_re = re.compile(r'order$', re.I)
+    return order_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(order_re_func())
