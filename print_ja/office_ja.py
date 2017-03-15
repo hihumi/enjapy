30e32d83d2f9d543354e3ceb90a4bdee31fe8325
hihumi <yui.maa5800@gmail.com>
2017-03-16 04:30:16 +0900

    add file

---
+++ print_ja/office_ja.py
@@ -0,0 +1,26 @@
+#!/use/bin/env python3
+
+
+"""officeの日本語意味を出力する
+"""
+
+
+def office_ja_func():
+    """officeの日本語意味を出力する関数
+
+    doctest:
+    >>> office_ja_func()
+    office:
+    [名]: [加算, 不加算]: (1) 事務所 (2) 職場 (3) 役所 (4) 案内所
+    """
+
+    print('office:')
+
+    office_ja_words = """[名]: [加算, 不加算]: (1) 事務所 (2) 職場 (3) 役所 (4) 案内所"""
+
+    print(office_ja_words)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    office_ja_func()
---
+++ re_en/office_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""officeの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def office_re_func():
+    """officeの正規表現をコンパイルする関数
+
+    return value: office_re
+
+    doctest:
+    >>> print(office_re_func())
+    re.compile('office$', re.IGNORECASE)
+    """
+
+    office_re = re.compile(r'office$', re.I)
+    return office_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(office_re_func())
