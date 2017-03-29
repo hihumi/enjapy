d7b04496d3c94c8dc91dd40042876b1d2c4d71a9
hihumi <yui.maa5800@gmail.com>
2017-03-30 08:34:47 +0900

    update

--- re_search_en/s_S_re_search_en.py
+++ re_search_en/s_S_re_search_en.py
@@ -12,12 +12,16 @@ sxxx_jaモジュールのsxxx_ja_func()を呼ぶ
 # from re_en.sxxx_re import sxxx_re_func
 # service:
 from re_en.service_re import service_re_func
+# store:
+from re_en.store_re import store_re_func
+
 
 # s: a b c d e f g h i j k l m n o p q r s t u v w x y z
 #
 # from print_ja.sxxx_ja import sxxx_ja_func
 # service:
 from print_ja.service_ja import service_ja_func
+from print_ja.store_ja import store_ja_func
 
 
 def s_S_re_search_en_func(s_S_word):
@@ -29,7 +33,7 @@ def s_S_re_search_en_func(s_S_word):
     # s: a b c d e f g h i j k l m n o p q r s t u v w x y z
     if service_re_func().search(s_S_word): # service
         service_ja_func()
-    #elif yyy_re_func().search(word): # yyy
-    #    yyy_ja_func()
+    elif store_re_func().search(s_S_word): # store
+        store_ja_func()
     else:
         print('not found...')
