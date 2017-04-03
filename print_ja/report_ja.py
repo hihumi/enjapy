e6e8e4e569f982e83c5560bb848118c04d7abed2
hihumi <yui.maa5800@gmail.com>
2017-04-03 09:48:51 +0900

    add file

---
+++ print_ja/report_ja.py
@@ -0,0 +1,28 @@
+#!/use/bin/env python3
+
+
+"""reportの日本語意味を出力する
+"""
+
+
+def report_ja_func():
+    """reportの日本語意味を出力する関数
+
+    doctest:
+    >>> report_ja_func()
+    report:
+    [名]: [加算, 不加算]: (1) 報告 (2) 報道 (3) 噂, 風説
+    [動]: (1) ...を報告する (2) ...を報道する
+    """
+
+    print('report:')
+
+    report_ja_words = """[名]: [加算, 不加算]: (1) 報告 (2) 報道 (3) 噂, 風説
+[動]: (1) ...を報告する (2) ...を報道する"""
+
+    print(report_ja_words)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    report_ja_func()
---
+++ re_en/report_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""reportの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def report_re_func():
+    """reportの正規表現をコンパイルする関数
+
+    return value: report_re
+
+    doctest:
+    >>> print(report_re_func())
+    re.compile('report$', re.IGNORECASE)
+    """
+
+    report_re = re.compile(r'report$', re.I)
+    return report_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(report_re_func())
