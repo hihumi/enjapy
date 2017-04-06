6933fcf2a592c4a4e51fb7d753b0575b8b1e0f07
hihumi <yui.maa5800@gmail.com>
2017-04-06 20:12:37 +0900

    update

--- re_search_en/b_B_re_search_en.py
+++ re_search_en/b_B_re_search_en.py
@@ -13,6 +13,9 @@ bxxx_jaモジュールのbxxx_ja_func()を呼ぶ
 # business:
 from re_en.business_re import business_re_func
 
+# b-list:
+from re_en.b_B_list_re import b_B_list_re_func
+
 # b: a b c d e f g h i j k l m n o p q r s t u v w x y z
 #
 # from print_ja.bxxx_ja import bxxx_ja_func
@@ -20,17 +23,21 @@ from re_en.business_re import business_re_func
 from print_ja.business_ja import business_ja_func
 
 
+# b-lsit:
+from print_en_lists.b_B_list import b_B_list_func
+
+
 def b_B_re_search_en_func(b_B_word):
     """main.pyで入力されたwordが、
     bxxx_reモジュールのbxxx_re_func()で作成した正規表現のb、またはBからはじまる英単語と合致した場合、
     bxxx_jaモジュールのbxxx_ja_func()を呼ぶ関数
+    ただし、最後のelifは、b_B_listモジュールのb_B_list_func()を呼ぶ
     """
 
     # b: a b c d e f g h i j k l m n o p q r s t u v w x y z
     if business_re_func().search(b_B_word): # business
         business_ja_func()
-    #elif yyy_re_func().search(b_B_word): # yyy
-    #    yyy_ja_func()
-    #elif :
+    elif b_B_list_re_func().search(b_B_word): # b-list
+        b_B_list_func()
     else:
         print('not found...')
