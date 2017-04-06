af9ca0750c0c9e35fbe85c1c10fecd5155a5e080
hihumi <yui.maa5800@gmail.com>
2017-04-06 21:54:07 +0900

    add file

---
+++ print_ja/base_ja.py
@@ -0,0 +1,28 @@
+#!/use/bin/env python3
+
+
+"""baseの日本語意味を出力する
+"""
+
+
+def base_ja_func():
+    """baseの日本語意味を出力する関数
+
+    doctest:
+    >>> base_ja_func()
+    base:
+    [名]: [加算, 不加算]: 基部, 基礎, 土台, 底, 基地, 塁 (備考: 基地, 塁として使用する場合, 不加算になることがある)
+    [動]: (be ~d onで)...に基づいている
+    """
+
+    print('base:')
+
+    base_ja_words = """[名]: [加算, 不加算]: 基部, 基礎, 土台, 底, 基地, 塁 (備考: 基地, 塁として使用する場合, 不加算になることがある)
+[動]: (be ~d onで)...に基づいている"""
+
+    print(base_ja_words)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    base_ja_func()
---
+++ re_en/base_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""baseの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def base_re_func():
+    """baseの正規表現をコンパイルする関数
+
+    return value: base_re
+
+    doctest:
+    >>> print(base_re_func())
+    re.compile('base$', re.IGNORECASE)
+    """
+
+    base_re = re.compile(r'base$', re.I)
+    return base_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(base_re_func())
