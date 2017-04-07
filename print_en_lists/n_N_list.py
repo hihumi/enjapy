8b96d60263b8162bb578b73174b1444486f86dbe
hihumi <yui.maa5800@gmail.com>
2017-04-07 17:25:25 +0900

    add file

---
+++ print_en_lists/n_N_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""nまたはNから始まる英単語のリストを出力する
+"""
+
+
+def n_N_list_func():
+    """nまたはNから始まる英単語のリストをソートし出力する関数
+
+    doctest:
+    >>> n_N_list_func()
+    n-list:
+        negative
+    """
+
+    print('n-list:')
+
+    # アルファベット順にソートされるので、リスト(配列)末尾に英単語を追加してくだけでいい
+    n_N_en_words = sorted(['negative'])
+
+    [print('    {}'.format(i)) for i in n_N_en_words]
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    n_N_list_func()
---
+++ re_en/n_N_list_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""n-listの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def n_N_list_re_func():
+    """n-listの正規表現をコンパイルする関数
+
+    return value: n_N_list_re
+
+    doctest:
+    >>> print(n_N_list_re_func())
+    re.compile('n-list$', re.IGNORECASE)
+    """
+
+    n_N_list_re = re.compile(r'n-list$', re.I)
+    return n_N_list_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(n_N_list_re_func())
