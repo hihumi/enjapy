d6266aa1d441cf1b1b8e20029ce8ffa6b1da2fea
hihumi <yui.maa5800@gmail.com>
2017-04-07 17:12:54 +0900

    add file

---
+++ print_en_lists/j_J_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""jまたはJから始まる英単語のリストを出力する
+"""
+
+
+def j_J_list_func():
+    """uまたはuから始まる英単語のリストをソートし出力する関数
+
+    doctest:
+    >>> j_J_list_func()
+    j-list:
+        job
+    """
+
+    print('j-list:')
+
+    # アルファベット順にソートされるので、リスト(配列)末尾に英単語を追加してくだけでいい
+    j_J_en_words = sorted(['job'])
+
+    [print('    {}'.format(i)) for i in j_J_en_words]
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    j_J_list_func()
---
+++ re_en/j_J_list_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""j-listの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def j_J_list_re_func():
+    """xxxの正規表現をコンパイルする関数
+
+    return value: j_J_list_re
+
+    doctest:
+    >>> print(j_J_list_re_func())
+    re.compile('j-list$', re.IGNORECASE)
+    """
+
+    j_J_list_re = re.compile(r'j-list$', re.I)
+    return j_J_list_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(j_J_list_re_func())
