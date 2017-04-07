d6a1bf02a051e04d0755c6e8b19dba4db64e2d7d
hihumi <yui.maa5800@gmail.com>
2017-04-07 17:37:32 +0900

    update

--- re_search_en/o_O_re_search_en.py
+++ re_search_en/o_O_re_search_en.py
@@ -4,6 +4,7 @@
 """main.pyで入力されたwordが、
 oxxx_reモジュールのoxxx_re_func()で作成した正規表現のo、またはOからはじまる英単語と合致した場合、
 oxxx_jaモジュールのoxxx_ja_func()を呼ぶ
+ただし、o-listまたはO-list(すべて大文字小文字は問わない)と入力された場合、o_O_listモジュールのo_O_list_func()を呼ぶ
 """
 
 
@@ -17,8 +18,12 @@ from re_en.order_re import order_re_func
 # outlook:
 from re_en.outlook_re import outlook_re_func
 
-# o: a b c d e f g h i j k l m n o p q r s t u v w x y z
+# 0-list:
+from re_en.o_O_list_re import o_O_list_re_func
 
+
+# o: a b c d e f g h i j k l m n o p q r s t u v w x y z
+#
 # from print_ja.oxxx_ja import oxxx_ja_func
 # office:
 from print_ja.office_ja import office_ja_func
@@ -28,20 +33,24 @@ from print_ja.order_ja import order_ja_func
 from print_ja.outlook_ja import outlook_ja_func
 
 
+from print_en_lists.o_O_list import o_O_list_func
+
+
 def o_O_re_search_en_func(o_O_word):
     """main.pyで入力されたwordが、
     oxxx_reモジュールのoxxx_re_func()で作成した正規表現のo、またはOからはじまる英単語と合致した場合、
     oxxx_jaモジュールのoxxx_ja_func()を呼ぶ関数
+    ただし、最後のelifは、o_O_listモジュールのo_O_list_func()を呼ぶ
     """
 
     # o: a b c d e f g h i j k l m n o p q r s t u v w x y z
     if office_re_func().search(o_O_word): # office
         office_ja_func()
-    elif order_re_func().search(o_O_word):
+    elif order_re_func().search(o_O_word): # order
         order_ja_func()
     elif outlook_re_func().search(o_O_word): # outlook
         outlook_ja_func()
-    # elif yyy_re_func().search(word): # yyy
-    #    yyy_ja_func()
+    elif o_O_list_re_func().search(o_O_word): # o-list
+        o_O_list_func()
     else:
         print('not found...')
