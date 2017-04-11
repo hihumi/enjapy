533f4272f0120b93bc21887a22fea21ad45f4617
hihumi <yui.maa5800@gmail.com>
2017-04-11 18:24:49 +0900

    add file

---
+++ print_ja/area_ja.py
@@ -0,0 +1,28 @@
+#!/use/bin/env python3
+
+
+"""areaの日本語意味を出力する
+"""
+
+
+def area_ja_func():
+    """areaの日本語意味を出力する関数
+
+    doctest:
+    >>> area_ja_func()
+    area:
+        [名] [複 ~s] (1) [U,C] 面積, 坪数, 建坪 (2) [C] 地域, 地帯, 地方
+        (3) [C] 広場, 空き地, 区域, 場所, 空間 (4) [C] 範囲, 分野, 領域
+    """
+
+    print('area:')
+
+    area_ja_words = """    [名] [複 ~s] (1) [U,C] 面積, 坪数, 建坪 (2) [C] 地域, 地帯, 地方
+    (3) [C] 広場, 空き地, 区域, 場所, 空間 (4) [C] 範囲, 分野, 領域"""
+
+    print(area_ja_words)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    area_ja_func()
---
+++ re_en/area_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""areaの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def area_re_func():
+    """areaの正規表現をコンパイルする関数
+
+    return value: area_re
+
+    doctest:
+    >>> print(area_re_func())
+    re.compile('area$', re.IGNORECASE)
+    """
+
+    area_re = re.compile(r'area$', re.I)
+    return area_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(area_re_func())
