2adc6366ef9bf26145bff6ac925c2db4c73b3ae4
hihumi <yui.maa5800@gmail.com>
2017-04-11 13:54:43 +0900

    add file

---
+++ print_ja/room_ja.py
@@ -0,0 +1,36 @@
+#!/use/bin/env python3
+
+
+"""roomの日本語意味を出力する
+"""
+
+
+def room_ja_func():
+    """roomの日本語意味を出力する関数
+
+    doctest:
+    >>> room_ja_func()
+    room:
+        [名] [複 ~s] (1) [C] 部屋, 質 (2) [U] 空間, 場所,
+        余地
+        (3) [U] 可能性, 機会 (4) (複数形で)貸し室, 下宿
+        (5) (the ~ (集合的に))部屋にいる人々, 集合した人々
+    <BLANKLINE>
+        [動] [自] 同居する, 下宿する, 間借りする
+    """
+
+    print('room:')
+
+    room_ja_words = """    [名] [複 ~s] (1) [C] 部屋, 質 (2) [U] 空間, 場所,
+    余地
+    (3) [U] 可能性, 機会 (4) (複数形で)貸し室, 下宿
+    (5) (the ~ (集合的に))部屋にいる人々, 集合した人々
+
+    [動] [自] 同居する, 下宿する, 間借りする"""
+
+    print(room_ja_words)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    room_ja_func()
---
+++ re_en/room_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""roomの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def room_re_func():
+    """roomの正規表現をコンパイルする関数
+
+    return value: room_re
+
+    doctest:
+    >>> print(room_re_func())
+    re.compile('room$', re.IGNORECASE)
+    """
+
+    room_re = re.compile(r'room$', re.I)
+    return room_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(room_re_func())
