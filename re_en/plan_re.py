0fc60e518b704c899bd63c44893cc0b11c71b08e
hihumi <yui.maa5800@gmail.com>
2017-04-11 13:15:04 +0900

    add file

---
+++ re_en/plan_re.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""planの正規表現をコンパイルする
+"""
+
+
+import re
+
+
+def plan_re_func():
+    """planの正規表現をコンパイルする関数
+
+    return value: plan_re
+
+    doctest:
+    >>> print(plan_re_func())
+    re.compile('plan$', re.IGNORECASE)
+    """
+
+    plan_re = re.compile(r'plan$', re.I)
+    return plan_re
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print(plan_re_func())
